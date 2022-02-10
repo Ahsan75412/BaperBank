2af903ca002ffe2802e66216bb983e894220b9c3
Ahsan Habib <ahsanpub16b@gmail.com>
2022-02-10 18:10:54 +0600

    Banking area and other section add done here!

---
+++ js/bank.js
@@ -0,0 +1,14 @@
+
+document.getElementById('login-submit').addEventListener('click' , function(){
+    //get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    //check mail and password
+    if(userEmail == 'ahsan@gmail.com' && userPassword == 'secret'){
+       window.location.href = 'banking.html';
+    }
+})
